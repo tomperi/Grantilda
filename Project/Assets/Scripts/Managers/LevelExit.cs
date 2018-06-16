6e582864d7de01460bd5baa7a9abc126fcd161ac
Amir Padan <amirpadan@gmail.com>
2018-06-16 19:20:43 +0300

    Fixed teleport animation.
    Grandma now teleports when she is on the coin itself (reduced colider's size)

--- Project/Assets/Scripts/Managers/LevelExit.cs
+++ Project/Assets/Scripts/Managers/LevelExit.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 
 public class LevelExit : MonoBehaviour {
 
-    private MoveToNextLevelScript moveToNextLevelScript;
-
     private void Start()
     {
         gameObject.GetComponent<BoxCollider>().isTrigger = true;
-        moveToNextLevelScript = FindObjectOfType<MoveToNextLevelScript>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            /*
-            gameObject.GetComponent<BoxCollider>().isTrigger = false;
-            moveToNextLevelScript.OnLevelComplete();
-            */
+            other.GetComponent<PlayerController>().StopAtPlace();
             other.GetComponentInChildren<SpriteController>().SetTeleportOut();
         }
     }
