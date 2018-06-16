ed7fe0ebe946648f1ed129974312c7a10d089899
ndanieli <ndanieli@gmail.com>
2018-06-16 21:05:48 +0300

    update logo and bridge sizes
    This reverts commit e0dec8d42f70b5b9062a360c8884f11d2a2f0efe.

---
+++ Project/Assets/Scripts/UI/Main Menu/LogoChangeAlpha.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogoChangeAlpha : MonoBehaviour {
+
+    private Image img;
+    private float min = 0.5f;
+    private float max = 0.8f;
+    private bool isGoingUp;
+
+	// Use this for initialization
+	void Start () {
+        img = GetComponent<Image>();
+        isGoingUp = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isGoingUp)
+        {
+            if (img.color.a < max)
+            {
+                Color clr = img.color;
+                float a = clr.a;
+                clr.a = (a + 0.001f);
+                img.color = clr;
+            }
+            else
+            {
+                isGoingUp = false;
+            }
+        }
+        else
+        {
+            if (img.color.a > min)
+            {
+                Color clr = img.color;
+                float a = clr.a;
+                clr.a = (a - 0.001f);
+                img.color = clr;
+            }
+            else
+            {
+                isGoingUp = true;
+            }
+        }
+	}
+}
