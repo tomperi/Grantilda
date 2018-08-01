b4468185b816c7c99c26a8ace1fd45425cc398ee
Tom Peri <tomp228@gmail.com>
2018-08-02 00:11:57 +0300

    New in-level UI implemented

--- Project/Assets/Scripts/UI/LevelUI.cs
+++ Project/Assets/Scripts/UI/LevelUI.cs
@@ -8,14 +8,13 @@ public class LevelUI : MonoBehaviour
     private UIManager m_UIManager;
     private GameObject PauseButton;
     private GameObject PauseMenu;
-    private GameObject ConfirmRestart;
-    private GameObject ConfirmExit;
 
     void Start()
     {
         // Init all objects
         m_UIManager = FindObjectOfType<UIManager>();
         GameObject levelUI = GameObject.Find("LevelUI");
+
         for (int i = 0; i < levelUI.transform.childCount; i++)
         {
             GameObject child = levelUI.transform.GetChild(i).gameObject;
@@ -27,12 +26,6 @@ public class LevelUI : MonoBehaviour
                 case ("PauseMenu"):
                     PauseMenu = child;
                     break;
-                case ("Confirm Restart"):
-                    ConfirmRestart = child;
-                    break;
-                case ("Confirm Exit"):
-                    ConfirmExit = child;
-                    break;
             }
         }
 
@@ -42,61 +35,16 @@ public class LevelUI : MonoBehaviour
         {
             switch (button.name)
             {
-                case "MainMenuButton":
-                    button.onClick.AddListener(delegate { DisplayConfirmExit(true); });
-                    break;
-                case "RestartButton":
-                    button.onClick.AddListener(delegate { DisplayConfirmRestart(true); });
-                    break;
-                case "PlayButton":
+                case "ResumeButton":
                     button.onClick.AddListener(delegate { DisplayPauseMenu(false); });
                     break;
-            }
-        }
-
-        Button[] confirmExitButtons = ConfirmExit.GetComponentsInChildren<Button>();
-
-        foreach (Button button in confirmExitButtons)
-        {
-            switch (button.name)
-            {
-                case "Back":
-                    button.onClick.AddListener(delegate { DisplayConfirmExit(false); });
-                    break;
-                case "Confirm":
+                case "MainMenuButton":
                     button.onClick.AddListener(delegate { m_UIManager.GoToMain(); });
                     break;
             }
         }
-
-        Button[] confirmRestartButtons = ConfirmRestart.GetComponentsInChildren<Button>();
-
-        foreach (Button button in confirmRestartButtons)
-        {
-            switch (button.name)
-            {
-                case "Back":
-                    button.onClick.AddListener(delegate { DisplayConfirmRestart(false); });
-                    break;
-                case "Confirm":
-                    button.onClick.AddListener(delegate { m_UIManager.RestartCurrentLevel(); });
-                    break;
-            }
-        }
-
+        
         PauseButton.GetComponent<Button>().onClick.AddListener(delegate { DisplayPauseMenu(true); });
-    }
-
-    public void DisplayConfirmExit(bool i_Display)
-    {
-        ConfirmExit.SetActive(i_Display);
-        PauseMenu.SetActive(!i_Display);
-    }
-
-    public void DisplayConfirmRestart(bool i_Display)
-    {
-        ConfirmRestart.SetActive(i_Display);
-        PauseMenu.SetActive(!i_Display);
     }
 
     public void DisplayPauseMenu(bool i_Display)
